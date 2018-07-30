8a55d8eaa0ff00848c61a12aec84416073b2cb95
Apoorva Lal <lal.apoorva@gmail.com>
2018-07-30 00:38:58 -0700

    update

--- R_script.R
+++ R_script.R
@@ -1,5 +1,6 @@
 ####################################################
 rm(list=ls())
+library(LalRUtils)
 load_or_install(c('tidyverse','magrittr','rio','data.table')) #, "lib2"))
 sessionInfo()
 ####################################################
