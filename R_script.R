58401d4ec465b1f2c688c0301037d16909de34a8
Apoorva Lal <lal.apoorva@gmail.com>
2018-07-31 22:07:03 -0700

    "reorganised"

--- R_script.R
+++
@@ -1,6 +0,0 @@
-####################################################
-rm(list=ls())
-library(LalRUtils)
-load_or_install(c('tidyverse','magrittr','rio','data.table')) #, "lib2"))
-sessionInfo()
-####################################################
