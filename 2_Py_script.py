f4daa254af18d08cc366bc548f50d8986e1b86ae
Apoorva Lal <lal.apoorva@gmail.com>
2020-03-05 12:38:09 -0800

    update

--- 2_Py_script.py
+++ 2_Py_script.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import glob
+from pathlib import Path
 import numpy as np
 import pandas as pd
 
--- 2_R_script.R
+++ 2_R_script.R
@@ -1,6 +1,6 @@
 ####################################################
 rm(list=ls())
 library(LalRUtils)
-load_or_install(c('tidyverse','magrittr','rio','data.table')) #, "lib2"))
+libreq(tidyverse,magrittr,rio,data.table) #, lib2)
 set.seed(42)
 ####################################################
