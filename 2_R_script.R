a0a30ff611d200d6bc4e2c288c15542b1c0956b6
Apoorva Lal <lal.apoorva@gmail.com>
2020-05-06 21:04:01 -0700

    update

--- 2_R_script.R
+++ 2_R_script.R
@@ -1,6 +1,7 @@
-####################################################
+#%% ####################################################
 rm(list=ls())
 library(LalRUtils)
-libreq(tidyverse,magrittr,rio,data.table) #, lib2)
+libreq(tidyverse, magrittr, rio, data.table, janitor) #, lib2)
+theme_set(lal_plot_theme())
 set.seed(42)
-####################################################
+#%% ####################################################
