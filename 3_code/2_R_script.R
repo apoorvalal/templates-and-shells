d336fb0ed1a11fd76b6d391fd7348972f240b89a
Apoorva Lal <lal.apoorva@gmail.com>
2020-12-12 11:38:51 -0800

    "reorganised; cleaned up cruft" "

--- 3_code/2_R_script.R
+++ 3_code/2_R_script.R
@@ -1,7 +1,9 @@
+#!/usr/bin/env Rscript
 # %% ####################################################
 rm(list=ls())
 library(LalRUtils)
-LalRUtils::libreq(tidyverse, data.table, stargazer2, lfe, rio, foreach, magrittr, janitor)
+LalRUtils::libreq(tidyverse, data.table, stargazer2, rlist,
+                  lfe, rio, foreach, magrittr, janitor)
 theme_set(lal_plot_theme())
 options(repr.plot.width=12, repr.plot.height=9)
 set.seed(42)
