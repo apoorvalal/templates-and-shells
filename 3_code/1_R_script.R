268d5c475af8becdbbd41d2cf17f58ddfeac8e35
Apoorva Lal <lal.apoorva@gmail.com>
2022-06-13 15:56:43 -0700

    "moved fig defaults into importable r script"

--- 3_code/1_R_script.R
+++ 3_code/1_R_script.R
@@ -1,15 +1,7 @@
 #!/usr/bin/env Rscript
 # %% ####################################################
 rm(list = ls())
-library(LalRUtils)
-libreq(data.table, ggplot2, fst, fixest, rio, foreach, lubridate, stringr,
-  janitor, tictoc, RColorBrewer, patchwork, RPushbullet, IRdisplay)
-theme_set(lal_plot_theme()) # add _d() for dark
-options(repr.plot.width=12, repr.plot.height=9, scipen=999,
-        ggplot2.discrete.fill   = brewer.pal(9, "Set1"),
-        ggplot2.discrete.colour = brewer.pal(9, "Set1"),
-        ggplot2.continuous.fill = "viridis",
-        ggplot2.continuous.colour = "viridis")
 set.seed(42)
-notif = \(x) pbPost("note", x)
+libreq(data.table, ggplot2, fixest, janitor)
+source("~/Templates/boilerplate/RFigDefaults.R")
 # %% ####################################################
